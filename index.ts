d98f87ff82a46ad0a384729fa309c356cf6e7791
netTrek <info@nettrek.de>
2024-01-18 08:40:15 +0100

    github projekt läuft jetzt auch in der Preview

--- index.ts
+++ index.ts
@@ -4,3 +4,7 @@ import './style.css';
 // Write TypeScript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const test = 123;
+
+let name = 'saban';
